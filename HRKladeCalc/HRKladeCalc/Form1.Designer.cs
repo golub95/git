d55035b197cf72cb0a4610d600a7639bb5ca9915
hretf <golub793@live.com>
2022-10-19 21:41:28 +0200

    Kalkulator

---
+++ HRKladeCalc/HRKladeCalc/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace HRKladeCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbKoef1 = new System.Windows.Forms.TextBox();
+            this.tbKoef2 = new System.Windows.Forms.TextBox();
+            this.lbPostoProfit = new System.Windows.Forms.Label();
+            this.lbUplata1 = new System.Windows.Forms.Label();
+            this.lbUplata2 = new System.Windows.Forms.Label();
+            this.tbUlog = new System.Windows.Forms.TextBox();
+            this.lbuplata1Text = new System.Windows.Forms.Label();
+            this.btnIzracunaj = new System.Windows.Forms.Button();
+            this.lbuplata2Text = new System.Windows.Forms.Label();
+            this.lbKoef1 = new System.Windows.Forms.Label();
+            this.lbKoef2 = new System.Windows.Forms.Label();
+            this.lbUlog = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbKoef1
+            // 
+            this.tbKoef1.Location = new System.Drawing.Point(88, 6);
+            this.tbKoef1.Name = "tbKoef1";
+            this.tbKoef1.Size = new System.Drawing.Size(100, 20);
+            this.tbKoef1.TabIndex = 0;
+            // 
+            // tbKoef2
+            // 
+            this.tbKoef2.Location = new System.Drawing.Point(88, 44);
+            this.tbKoef2.Name = "tbKoef2";
+            this.tbKoef2.Size = new System.Drawing.Size(100, 20);
+            this.tbKoef2.TabIndex = 1;
+            // 
+            // lbPostoProfit
+            // 
+            this.lbPostoProfit.AutoSize = true;
+            this.lbPostoProfit.Location = new System.Drawing.Point(319, 90);
+            this.lbPostoProfit.Name = "lbPostoProfit";
+            this.lbPostoProfit.Size = new System.Drawing.Size(0, 13);
+            this.lbPostoProfit.TabIndex = 2;
+            // 
+            // lbUplata1
+            // 
+            this.lbUplata1.AutoSize = true;
+            this.lbUplata1.Location = new System.Drawing.Point(319, 9);
+            this.lbUplata1.Name = "lbUplata1";
+            this.lbUplata1.Size = new System.Drawing.Size(0, 13);
+            this.lbUplata1.TabIndex = 3;
+            // 
+            // lbUplata2
+            // 
+            this.lbUplata2.AutoSize = true;
+            this.lbUplata2.Location = new System.Drawing.Point(319, 52);
+            this.lbUplata2.Name = "lbUplata2";
+            this.lbUplata2.Size = new System.Drawing.Size(0, 13);
+            this.lbUplata2.TabIndex = 4;
+            // 
+            // tbUlog
+            // 
+            this.tbUlog.Location = new System.Drawing.Point(88, 87);
+            this.tbUlog.Name = "tbUlog";
+            this.tbUlog.Size = new System.Drawing.Size(100, 20);
+            this.tbUlog.TabIndex = 7;
+            // 
+            // lbuplata1Text
+            // 
+            this.lbuplata1Text.AutoSize = true;
+            this.lbuplata1Text.Location = new System.Drawing.Point(226, 9);
+            this.lbuplata1Text.Name = "lbuplata1Text";
+            this.lbuplata1Text.Size = new System.Drawing.Size(64, 13);
+            this.lbuplata1Text.TabIndex = 18;
+            this.lbuplata1Text.Text = "Uplata za 1:";
+            // 
+            // btnIzracunaj
+            // 
+            this.btnIzracunaj.Location = new System.Drawing.Point(215, 85);
+            this.btnIzracunaj.Name = "btnIzracunaj";
+            this.btnIzracunaj.Size = new System.Drawing.Size(75, 23);
+            this.btnIzracunaj.TabIndex = 21;
+            this.btnIzracunaj.Text = "Izracunaj";
+            this.btnIzracunaj.UseVisualStyleBackColor = true;
+            this.btnIzracunaj.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbuplata2Text
+            // 
+            this.lbuplata2Text.AutoSize = true;
+            this.lbuplata2Text.Location = new System.Drawing.Point(226, 52);
+            this.lbuplata2Text.Name = "lbuplata2Text";
+            this.lbuplata2Text.Size = new System.Drawing.Size(64, 13);
+            this.lbuplata2Text.TabIndex = 22;
+            this.lbuplata2Text.Text = "Uplata za 2:";
+            // 
+            // lbKoef1
+            // 
+            this.lbKoef1.AutoSize = true;
+            this.lbKoef1.Location = new System.Drawing.Point(12, 9);
+            this.lbKoef1.Name = "lbKoef1";
+            this.lbKoef1.Size = new System.Drawing.Size(68, 13);
+            this.lbKoef1.TabIndex = 23;
+            this.lbKoef1.Text = "Koeficijent 1:";
+            // 
+            // lbKoef2
+            // 
+            this.lbKoef2.AutoSize = true;
+            this.lbKoef2.Location = new System.Drawing.Point(12, 47);
+            this.lbKoef2.Name = "lbKoef2";
+            this.lbKoef2.Size = new System.Drawing.Size(68, 13);
+            this.lbKoef2.TabIndex = 24;
+            this.lbKoef2.Text = "Koeficijent 2:";
+            // 
+            // lbUlog
+            // 
+            this.lbUlog.AutoSize = true;
+            this.lbUlog.Location = new System.Drawing.Point(12, 90);
+            this.lbUlog.Name = "lbUlog";
+            this.lbUlog.Size = new System.Drawing.Size(32, 13);
+            this.lbUlog.TabIndex = 25;
+            this.lbUlog.Text = "Ulog:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 133);
+            this.Controls.Add(this.lbUlog);
+            this.Controls.Add(this.lbKoef2);
+            this.Controls.Add(this.lbKoef1);
+            this.Controls.Add(this.lbuplata2Text);
+            this.Controls.Add(this.btnIzracunaj);
+            this.Controls.Add(this.lbuplata1Text);
+            this.Controls.Add(this.tbUlog);
+            this.Controls.Add(this.lbUplata2);
+            this.Controls.Add(this.lbUplata1);
+            this.Controls.Add(this.lbPostoProfit);
+            this.Controls.Add(this.tbKoef2);
+            this.Controls.Add(this.tbKoef1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbKoef1;
+        private System.Windows.Forms.TextBox tbKoef2;
+        private System.Windows.Forms.Label lbPostoProfit;
+        private System.Windows.Forms.Label lbUplata1;
+        private System.Windows.Forms.Label lbUplata2;
+        private System.Windows.Forms.TextBox tbUlog;
+        private System.Windows.Forms.Label lbuplata1Text;
+        private System.Windows.Forms.Button btnIzracunaj;
+        private System.Windows.Forms.Label lbuplata2Text;
+        private System.Windows.Forms.Label lbKoef1;
+        private System.Windows.Forms.Label lbKoef2;
+        private System.Windows.Forms.Label lbUlog;
+    }
+}
+
---
+++ HRKladeCalc/HRKladeCalc/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRKladeCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(tbKoef1.Text, out double koef1) && double.TryParse(tbKoef2.Text, out double koef2) && double.TryParse(tbUlog.Text, out double ulog))
+            {
+                double izracun1 = 1 / (0.95 * koef1 - 0.095);
+                double izracun2 = 1 / (0.95 * koef2 - 0.095);
+
+                lbPostoProfit.Text = ((1 - (izracun1 + izracun2)) * 100).ToString() + " %";
+
+                double uplata1 = ulog * izracun1;
+                double uplata2 = ulog * izracun2;
+
+                lbUplata1.Text = uplata1.ToString();
+                lbUplata2.Text = uplata2.ToString();
+
+                /*
+                double z1 = (0.95 * x * y - 0.095 * y);
+                lbuplata1Text.Text = (z1).ToString();
+                double calc = z1 / Math.Pow((0.95 * x - 0.095), 2);
+                label14.Text = (calc).ToString();
+                label15.Text = ((0.95 * x * calc - 0.095 * calc)).ToString();*/
+            }
+        }
+    }
+}
